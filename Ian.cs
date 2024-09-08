850c31b6ca13a81935ef768a567f090fceef5bfa
0x4D2 <156579141+Yweiwu03@users.noreply.github.com>
2024-09-08 22:48:30 +0800

    其他

--- Ian.cs
+++ Ian.cs
@@ -12,14 +12,15 @@ namespace Revalian
 {
     public partial class Ian
     {
-        public static void AddNewSprite(RDLevel chart, string spritePath, byte room, bool visibility, int depth, PointE pos, PointE scale)
+        public static void AddNewSprite(RDLevel chart, string spritePath, byte room, bool visibility, int depth, PointE pos, PointE scale,
+            float? angle, PointE pivot)
         {
             Sprite sprite = new(spritePath);
             DecorationEventCollection deco = chart.CreateDecoration(new SingleRoom(room), sprite);
             deco.Depth = depth;
             deco.Visible = visibility;
-            deco.Add(new Move() { Beat = chart.Calculator.BeatOf(1), Position = pos, Scale = scale});
-            deco.Add(new Tint() { Beat = chart.Calculator.BeatOf(1) });
+            deco.Add(new Move() { Beat = chart.Calculator.BeatOf(1), Position = pos, Scale = scale, Angle = angle, Pivot = pivot});
+            deco.Add(new Tint() { Beat = chart.Calculator.BeatOf(1), Opacity = 100 });
             deco.Add(new Tile() { Beat = chart.Calculator.BeatOf(1) });
         }
     }
